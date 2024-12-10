974c54f82e5418f15cd6db41c96486e2554db8d5
Emre Yesilyurt <emre@Emres-MacBook-Pro.local>
2024-12-10 15:56:53 +0700

    Update website with new Bootstrap theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,229 +1,303 @@
+'use strict';
+
+(function () {
+"use strict";
+
 /**
-* Template Name: DevFolio - v4.7.1
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
-  "use strict";
-
-  /**
-   * Easy selector helper function
+   * [isMobile description]
+   * @type {Object}
    */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
 
-  /**
-   * Easy event listener function
+window.isMobile = {
+	Android: function Android() {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function BlackBerry() {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function iOS() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function Opera() {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function Windows() {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function any() {
+		return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+	}
+};
+window.isIE = /(MSIE|Trident\/|Edge\/)/i.test(navigator.userAgent);
+window.windowHeight = window.innerHeight;
+window.windowWidth = window.innerWidth;
+
+/**
+   * Match height 
    */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
+$('.row-eq-height > [class*="col-"]').matchHeight();
 
-  /**
-   * Easy on scroll event listener 
+var myEfficientFn = debounce(function () {
+	$('.row-eq-height > [class*="col-"]').matchHeight();
+}, 250);
+
+window.addEventListener('resize', myEfficientFn);
+
+/**
+   * [debounce description]
+   * @param  {[type]} func      [description]
+   * @param  {[type]} wait      [description]
+   * @param  {[type]} immediate [description]
+   * @return {[type]}           [description]
    */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function () {
+		var context = this,
+			    args = arguments;
+		var later = function later() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
 
-  /**
-   * Navbar links active state on scroll
+/**
+   * Fullscreen menu
    */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+$('.fullscreenmenu__module').each(function () {
+	var self = $(this),
+		    triggerID = self.attr('trigger');
 
-  /**
-   * Scrolls to an element with header offset
+	self.on("click", function () {
+		$(triggerID).toggleClass('open');
+		$(this).toggleClass('open');
+	});
+	$(triggerID).on("click", function () {
+		$(triggerID).toggleClass('open');
+		self.toggleClass('open');
+	});
+});
+
+/**
+   * Masonry
    */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
+$('.grid__inner').masonry({
+	itemSelector: '.grid-item',
+	columnWidth: '.grid-sizer'
+});
 
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 16
-    }
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
+/**
+   * grid css
    */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
 
-  /**
-   * Back to top button
+$.fn.reCalWidth = function () {
+	var $self = $(this);
+	$self.on('reCalWidth', function () {
+		var _self = $(this);
+		_self.css('width', '');
+		var width = Math.floor(_self.width());
+		_self.css('width', width + 'px');
+		var height = Math.floor(_self.parent().children('.wide').width() / 2);
+		_self.parent().children('.wide').css('height', height + 'px');
+	});
+	$(window).on('resize', function () {
+		$self.trigger('reCalWidth');
+	});
+};
+function work() {
+	$('.grid-css').each(function () {
+		var workWrapper = $(this),
+			    workContainer = $('.grid__inner', workWrapper),
+			    filters = $('.filter', workWrapper),
+			    filterCurrent = $('.current a', filters),
+			    filterLiCurrent = $('.current', filters),
+			    duration = 0.3;
+		workContainer.imagesLoaded(function () {
+
+			// Fix Height
+			if (workWrapper.hasClass('grid-css--fixheight')) {
+				workContainer.find('.grid-item__content-wrapper').matchHeight();
+			}
+
+			workContainer.isotope({
+				layoutMode: 'masonry',
+				itemSelector: '.grid-item',
+				transitionDuration: duration + 's',
+				masonry: {
+					columnWidth: '.grid-sizer'
+					// hiddenStyle: {},
+					// visibleStyle: {}
+				} });
+		});
+		filters.on('click', 'a', function (e) {
+			e.preventDefault();
+			var $el = $(this);
+			var selector = $el.attr('data-filter');
+			filters.find('.current').removeClass('current');
+			$el.parent().addClass('current');
+			workContainer.isotope({
+				filter: selector
+			});
+		});
+
+		filters.find('.select-filter').change(function () {
+			var $el = $(this);
+			var selector = $el.val();
+			workContainer.isotope({
+				filter: selector
+			});
+		});
+
+		$('.grid-item', workWrapper).reCalWidth();
+	});
+}
+work();
+
+$('.ef-hoverdir').each(function () {
+	$(this).hoverdir({
+		speed: 300,
+		easing: 'ease',
+		hoverDelay: 40
+	});
+});
+
+/**
+   * ProgressBar
    */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
+var progress = $('.progress');
 
-  /**
-   * Mobile nav toggle
+progress.each(function () {
+
+	var _self = $(this);
+	var progressNumber = _self.find('.progress__number');
+	progressNumber.text('0%');
+
+	_self.waypoint(function (direction) {
+		var progressBar = _self.find('.progress__bar'),
+			    delay = progressBar.data("delay"),
+			    durations = progressBar.data("duration"),
+			    timing = progressBar.data("timing"),
+			    getPercent = progressBar.data('progress-percent');
+
+		console.log(durations);
+
+		progressBar.css({
+			'width': getPercent + '%',
+			'transition': 'all ' + durations + 'ms ' + timing,
+			'transition-delay': delay + 'ms'
+		});
+
+		setTimeout(function () {
+			progressNumber.prop('Counter', 0).animate({
+				Counter: getPercent
+			}, {
+				duration: durations,
+				easing: 'swing',
+				step: function step(now) {
+					$(this).text(Math.ceil(now) + '%');
+				}
+			});
+		}, delay);
+
+		this.destroy();
+	}, {
+		offset: function offset() {
+			return Waypoint.viewportHeight() - _self.outerHeight() - 150;
+		}
+	});
+});
+
+/**
+   * Typing effect
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+$('.typing__module').each(function (index) {
+	var self = $(this),
+		    _wrapper = $('.typed', self)[0],
+		    optData = eval('(' + self.attr('data-options') + ')'),
+		    optDefault = {
+		stringsElement: self.find('.typed-strings')[0],
+		typeSpeed: 50,
+		backSpeed: 500,
+		fadeOut: true,
+		loop: true
+	},
+		    options = $.extend(optDefault, optData);
+	var typed = new Typed(_wrapper, options);
+});
 
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+/**
+  * Footer
+  */
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+$('#back-to-top').on('click', function (e) {
+	e.preventDefault();
+	$('html,body').animate({
+		scrollTop: 0
+	}, 700);
+});
+//*
+// Header
+//*
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
+var wh = $(window).height(),
+	    half = wh / 5,
+	    headerHeight = $('header').outerHeight();
 
-  /**
-   * Intro type effect
-   */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 35,
-      backSpeed: 23,
-      backDelay: 600
-    });
-  }
+$(window).scroll(function () {
+	var scrollTop = $(window).scrollTop();
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
+	if (scrollTop >= half) {
+		$('header').addClass('is-scroll');
+	} else {
+		$('header').removeClass('is-scroll');
+	}
+});
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+$('.onepage-nav').dropdownMenu({
+	menuClass: 'onepage-menu',
+	breakpoint: 1200,
+	toggleClass: 'active',
+	classButtonToggle: 'navbar-toggle',
+	subMenu: {
+		class: 'sub-menu',
+		parentClass: 'menu-item-has-children',
+		toggleClass: 'active'
+	}
+});
 
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+$('.onepage-nav').onePageNav({
+	currentClass: 'current-menu-item',
+	scrollOffset: headerHeight
+});
 
-  /**
-   * Preloader
-   */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+//*
+// Back to top
+//*
 
-})()
+$(window).scroll(function () {
+	var wh = $(window).height(),
+		    scrollTop = $(window).scrollTop();
+
+	if (scrollTop >= wh) {
+		$('#back-to-top').addClass('is-visible');
+	} else {
+		$('#back-to-top').removeClass('is-visible');
+	}
+});
+
+var headerHeight = $('header').outerHeight();
+
+$('#back-to-down').on('click', function () {
+	var offsets = $(this).closest('.hero').next().offset().top - headerHeight;
+
+	$('html,body').animate({
+		scrollTop: offsets
+	}, 700);
+});
+})();
